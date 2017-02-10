098005617d7f9fa16892789e7fa410ca8e975100
Jordan Demmouche <jordand75020@hotmail.fr>
2017-02-10 11:46:47 +0100

    add MVP features

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
+ruby '2.3.3'
 source 'https://rubygems.org'
 
 git_source(:github) do |repo_name|
@@ -46,6 +47,8 @@ gem "autoprefixer-rails"
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'better_errors'
+  gem 'binding_of_caller'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-
-  resources :cocktails
+  resources :cocktails, only: [:show, :index, :new, :create] do
+    resources :doses, only: :create
+  end
+  resources :doses, only: :destroy
+  resources :ingredients, only: :show
   root "cocktails#index"
 end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

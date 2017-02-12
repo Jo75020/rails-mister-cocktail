485763e5f9d39d67aedb797ddbf25eef97b4fae0
Jordan Demmouche <jordand75020@hotmail.fr>
2017-02-12 18:21:33 +0100

    new db:seed

--- db/schema.rb
+++ db/schema.rb
@@ -25,15 +25,6 @@ ActiveRecord::Schema.define(version: 20170210110957) do
     t.string   "photo"
   end
 
-  create_table "coktails", force: :cascade do |t|
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.integer  "rating",      default: 0, null: false
-    t.string   "name"
-    t.string   "ingredient"
-    t.text     "description"
-  end
-
   create_table "doses", force: :cascade do |t|
     t.text     "description"
     t.datetime "created_at",    null: false

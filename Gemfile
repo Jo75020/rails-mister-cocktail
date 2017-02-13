5764a486d54d2c0e925f6e67acf3b9ba5e2f1304
Jordan Demmouche <jordand75020@hotmail.fr>
2017-02-13 09:19:57 +0100

    add seed

--- Gemfile
+++ Gemfile
@@ -21,7 +21,6 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
@@ -43,6 +42,12 @@ gem "simple_form"
 gem "autoprefixer-rails"
 gem 'cloudinary', '1.1.0'
 gem 'figaro'
+
+
+
+require "json"
+require 'open-uri'
+require "rest-client"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,95 +1,24 @@
 Cocktail.destroy_all
 Ingredient.destroy_all
 
-cocktails = [
-  {
-    name: "Old Fashioned",
-    picture: "http://www.seriouseats.com/images/2014/11/20141104-cocktail-party-old-fashioneds-holiday-vicky-wasik-3.jpg"
-    },
-  {
-    name: "Daiquiri",
-    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-daiquiri.jpg"
-    },
-  {
-    name: "Margarita",
-    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-margarita.jpg"
-    },
-  {
-    name: "Sidecar",
-    picture: "http://www.seriouseats.com/images/2014/11/20141101-cognac-sidecar-carey-jones.jpg"
-    },
-  {
-    name: "French 75",
-    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-french75.jpg"
-    },
-  {
-    name: "Bloody Mary",
-    picture: "http://www.seriouseats.com/images/2015/03/twase-20150320-21.jpg"
-    },
-  {
-    name: "Irish Coffee",
-    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-irish-coffee.jpg"
-    },
-  {
-    name: "Jack Rose",
-    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-jack-rose.jpg"
-    },
-  {
-    name: "Negroni",
-    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-negroni.jpg"
-    },
-  {
-    name: "Boulevardier",
-    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-boulevardier.jpg"
-    },
-  {
-    name: "Sazerac",
-    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-sazerac.jpg"
-    },
-  {
-    name: "Vieux Carré",
-    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-vieux-carre.jpg"
-    },
-  {
-    name: "Ramos Gin Fizz",
-    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-robyn-lee-ramos-gin-fizz.jpg"
-    },
-  {
-    name: "Mint Julep",
-    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-mint-julep.jpg"
-    },
-  {
-    name: "Whiskey Sour",
-    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-whiskey-sour.jpg"
-    },
-  {
-    name: "Mai Tai",
-    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-mai-tai.jpg"
-    },
-  {
-    name: "Planter's Punch",
-    picture: "http://www.seriouseats.com/images/2015/04/20150406-cocktails-planters-punch-robyn-lee-1.jpg"
-    },
-  {
-    name: "Pisco Sour",
-    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-pisco-sour.jpg"
-    },
-  {
-    name: "Cosmopolitan",
-    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-cosmopolitan.jpg"
-    },
-  {
-    name: "Tom Collins",
-    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-tom-collins.jpg"
-    },
-  {
-    name: "Last Word",
-    picture: "http://www.seriouseats.com/images/2015/03/20150323-cocktails-vicky-wasik-last-word.jpg"
-  }
-]
+url =  'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+cok = user["drinks"]
+cok.each do |x|
+  ingredients = Ingredient.create(name: x["strIngredient1"])
+end
 
-ingredients = %w(lemon ice mint leaves redbull jagermeister sugar tonic gin rhum)
-ingredients.each { |ingredient| Ingredient.create(name: ingredient) }
-
-
-cocktails.each { |cocktail| Cocktail.create(cocktail) }
+url_c =  'http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail'
+user_serialized_c = open(url_c).read
+user_c = JSON.parse(user_serialized_c)
+cok_c = user_c["drinks"]
+cok_c.each do |x|
+  c = Cocktail.new(name: x["strDrink"].to_s, picture: x["strDrinkThumb"])
+  if c.picture.nil?
+    c.picture = "https://unsplash.it/200/300/?random"
+    c.save!
+  else
+    c.save!
+  end
+end
